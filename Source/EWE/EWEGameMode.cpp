7c52fb6416f45971e08409714cda869ea43f38a9
Sophriel <32302066+Sophriel@users.noreply.github.com>
2023-05-20 21:26:26 +0900

    Initial Commit
    Ignored files without Sources

---
+++ Source/EWE/EWEGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EWEGameMode.h"
+#include "EWECharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AEWEGameMode::AEWEGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/EWE/EWEGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EWEGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEWEGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEWEGameMode();
+};
+
+
+
