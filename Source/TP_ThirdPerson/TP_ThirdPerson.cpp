faaa01111a8793aca98a4cb199c69c366c709d0f
Sophriel <32302066+Sophriel@users.noreply.github.com>
2023-05-20 21:00:21 +0900

    Remove 4.25
    Remove old version 4.25 for rework with 5.2

--- Source/TP_ThirdPerson/TP_ThirdPerson.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TP_ThirdPerson.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_GAME_MODULE( FDefaultGameModuleImpl, TP_ThirdPerson );
